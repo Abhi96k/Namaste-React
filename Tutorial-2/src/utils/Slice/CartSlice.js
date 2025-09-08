77c92bfac495413adab1174c6abd3c008d7d8412
abhiDhantellius <abhishek.nangare@tellius.com>
2025-09-09 00:30:50 +0530

    chore: Add comments to clarify Redux Toolkit usage and immutability handling

--- Tutorial-2/src/utils/Slice/CartSlice.js
+++ Tutorial-2/src/utils/Slice/CartSlice.js
@@ -39,3 +39,18 @@ const cardSlice = createSlice({
 export default cardSlice.reducer;
 export const { addItem, removeItem, clearCart, updateQuantity } =
   cardSlice.actions;
+
+
+
+  //RTK-either mutate the state directly or return a new state
+  //RTK uses immer library under the hood to handle immutability
+  //immer library allows us to write mutating logic in reducers
+  //but it doesn't actually mutate the state
+  //it creates a new state based on the mutations we made
+  //and returns the new state
+  //so we can write mutating logic in reducers without worrying about immutability
+  //immer library is included in redux toolkit by default
+  //we don't need to install it separately
+  //we can use immer library directly if we want to
+  //but it's not recommended as we are using redux toolkit
+  //which already includes immer library 
